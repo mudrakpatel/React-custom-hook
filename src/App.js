0e99617c155dc8573b76a889edfa0e03bae6964c
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-12 14:06:54 -0400

    useInput Custom Hook

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import './App.css';
 //import DocTitleOne from './components/DocTitleOne';
 //import DocTitleTwo from './components/DocTitleTwo';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
+//import CounterOne from './components/CounterOne';
+//import CounterTwo from './components/CounterTwo';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
     <div className="App">
       {/*<DocTitleOne/>
       <DocTitleTwo/>*/}
-      <CounterOne/>
-      <CounterTwo/>
+      {/*<CounterOne/>
+      <CounterTwo/>*/}
+      <UserForm/>
     </div>
   );
 }
