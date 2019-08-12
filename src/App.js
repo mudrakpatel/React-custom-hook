59040787d8f303b6125e74a1892c1e7f1975f50d
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-12 12:55:49 -0400

    useCounter Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import DocTitleOne from './components/DocTitleOne';
-import DocTitleTwo from './components/DocTitleTwo';
+//import DocTitleOne from './components/DocTitleOne';
+//import DocTitleTwo from './components/DocTitleTwo';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <DocTitleOne/>
-      <DocTitleTwo/>
+      {/*<DocTitleOne/>
+      <DocTitleTwo/>*/}
+      <CounterOne/>
+      <CounterTwo/>
     </div>
   );
 }
